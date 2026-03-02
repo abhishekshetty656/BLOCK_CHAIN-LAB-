7ebfd47ac4a7d166fe9cd19423a6119d09249459
Abhishek_7 <abhisheknshetty656@gmail.com>
2026-03-02 10:54:28 +0530

    Create Square.sol

---
+++ Square.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SquareCalculator {
+
+    function square(uint256 number) public pure returns (uint256) {
+        return number * number;
+    }
+}
